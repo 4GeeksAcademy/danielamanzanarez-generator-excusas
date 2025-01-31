d0b17dfc108f3c76ee1652bd0520426f6b43957f
Danielamanzanarez <danieladcmm92@gmail.com>
2025-01-31 18:57:07 +0000

    generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+console.log("hola");
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //write your code here // escribe tu codigo aqui
+  let who = ["The dog ", "My grandma ", "The mailman ", "My bird "];
+  let action = ["ate ", "peed ", "crushed ", "broke "];
+  let what = ["my homework ", "my phone ", "the car "];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoExAleatoria = Math.floor(Math.random() * who.length);
+  let actionExAleatoria = Math.floor(Math.random() * action.length);
+  let whatExAleatoria = Math.floor(Math.random() * what.length);
+  let whenExAleatoria = Math.floor(Math.random() * when.length);
+  document.querySelector("#excusas").innerHTML =
+    who[whoExAleatoria] +
+    action[actionExAleatoria] +
+    what[whatExAleatoria] +
+    when[whenExAleatoria];
 };
